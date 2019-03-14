b17b4e1ca13f9eb601c2aa04293e834dfa1eccc2
Ifeanyi Osinakayah Joseph <ifeanyiosinakayah15@gmail.com>
2019-03-14 10:49:57 +0100

    done

--- resources/coreui/src/config/index.js
+++ resources/coreui/src/config/index.js
@@ -2,4 +2,4 @@
  * Created by proteux on 5/20/18.
  */
 // export const BASE_URL = 'http://sendchow.me'
-export const BASE_URL = 'http://localhost:8000'
+export const BASE_URL = 'https://enigmatic-bastion-54924.herokuapp.com'
