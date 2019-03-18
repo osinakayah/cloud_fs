de629047567dabf210f26596dc6b8965d848befc
Ifeanyi Osinakayah Joseph <ifeanyiosinakayah15@gmail.com>
2019-03-18 13:03:30 +0100

    Removed google cloud bucket config from the filesystem.php

--- config/filesystems.php
+++ config/filesystems.php
@@ -62,13 +62,6 @@ return [
             'region' => env('AWS_DEFAULT_REGION'),
             'bucket' => env('AWS_BUCKET'),
         ],
-        'gcs' => [
-            'driver' => 'gcs',
-            'project_id' => env('GOOGLE_CLOUD_PROJECT_ID', 'your-project-id'),
-            'key_file' => env('GOOGLE_CLOUD_KEY_FILE', storage_path('app/cloud-fs-f8764a0cd3a4.json')), // optional: /path/to/service-account.json
-            'bucket' => env('GOOGLE_CLOUD_STORAGE_BUCKET', 'your-bucket'),
-            'storage_api_uri' => env('GOOGLE_CLOUD_STORAGE_API_URI', null), // see: Public URLs below
-        ],
 
     ],
 
